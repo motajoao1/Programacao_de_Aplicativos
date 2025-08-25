46cb442c9a56c11baae0eb0f37b7572f2f765c73
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2025-08-25 16:51:14 -0300

    segundo

---
+++ src/App.js
@@ -0,0 +1,36 @@
+// projeto\src\App.js
+
+import React, { useState } from 'react';
+import './App.css';
+import logo from  '../src/assets/images/logo_resident.png'
+
+import AdicionarProduto from './components/AdicionarProduto';
+import ListaDeProdutos from './components/ListaDeProdutos';
+
+function App() {
+  const [produtos, setProdutos] = useState(['Mouse', 'Teclado', 'Monitor']);
+
+  const adicionarUsuario = (nome) => {
+    const produto = nome.trim();
+
+    if (produtos.includes(produto)) {
+      alert('O produto já existe!');
+      return;
+    }  
+    setProdutos([...produtos, produto]);
+  };
+
+  return (
+    <div className="App">
+      <img src={logo} className="logo" alt="Logo do Resident " />
+      <h1>Produtos</h1>
+      <AdicionarProduto onAdd={adicionarUsuario} />
+      <hr />
+
+      <h2>Lista de produtos</h2>
+      <ListaDeProdutos itens={produtos} />
+    </div>
+  );
+}
+
+export default App;
